9bc78e2eec6d982ca88620631d9d2fe6929410d0
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2025-06-09 19:38:58 -0300

    Initial Commit

---
+++ memoryGame/memoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoryGameApp.swift
+//  memoryGame
+//
+//  Created by Ana Carolina  Mauro Ribeiro on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct memoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
