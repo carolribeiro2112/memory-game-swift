5d926d65e59ec08ca1f52f02a97fcc615456de95
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2025-06-25 13:47:31 -0300

    use Array to display the cards using ForEach

--- memoryGame/ContentView.swift
+++ memoryGame/ContentView.swift
@@ -5,15 +5,20 @@
 //  Created by Ana Carolina  Mauro Ribeiro on 09/06/25.
 //
 
+//alternative ways of typing an array
+// let emojis: Array<String> = ["👻","🎃","🕷️","😈"]
+// let emojis: [String] = ["👻","🎃","🕷️","😈"]
+
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["👻","🎃","🕷️","😈"]
+    
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+            ForEach(emojis.indices, id: \.self){index in
+                CardView(content:emojis[index])
+            }
         }
        
         .foregroundColor(.orange)
@@ -22,6 +27,7 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    let content: String
     @State var isFaceUp: Bool = false
     var body: some View {
         ZStack {
@@ -29,7 +35,7 @@ struct CardView: View {
             if isFaceUp{
                 base.fill(.white)
                 base.strokeBorder(style: StrokeStyle(lineWidth: 2), antialiased: true)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 base.fill()
             }
